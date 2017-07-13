3c586026c59d02e5cd7174bbc91e6d04fad61fa9
TongYoungRu <TongYoungRu@TongYoungRutekiMacBook-Pro.local>
2017-07-13 20:29:07 +0800

    1.廣告牆修改 2.功能順序修改

--- AgriBank/UI/View/FeatureWallView.swift
+++ AgriBank/UI/View/FeatureWallView.swift
@@ -12,19 +12,24 @@ protocol FeatureWallViewDelegate {
     func clickFeatureBtn(_ ID: PlatformFeatureID)
 }
 
-class FeatureWallView: UIView {
-    let imageView = UIImageView()
+let FeatureWall_PageControl_BottomDistance:CGFloat = 30
+let FeatureWall_PageControl_currentPageColor = UIColor(red: 69/255, green: 166/255, blue: 108/255, alpha:1)
+let FeatureWall_PageControl_PageColor = UIColor(red: 69/255, green: 166/255, blue: 108/255, alpha:0.2)
+
+class FeatureWallView: UIView, UIScrollViewDelegate {
     let scrollview = UIScrollView()
+    var pageCon = UIPageControl()
     var featureIDList = [PlatformFeatureID]()
     var featureDelegate:FeatureWallViewDelegate? = nil
     var vertical = 0
     var horizontal = 0
     
     override func layoutSubviews() {
-        imageView.frame = frame
-        imageView.frame.origin = .zero
         scrollview.frame = frame
+        scrollview.frame.size.height = frame.size.height - FeatureWall_PageControl_BottomDistance/2
         scrollview.frame.origin = .zero
+        pageCon.frame.origin.y = frame.size.height - FeatureWall_PageControl_BottomDistance
+        pageCon.frame.origin.x = frame.size.width/2 - pageCon.frame.size.width/2
         setFeatureWall(vertical, horizontal)
     }
     
@@ -34,33 +39,37 @@ class FeatureWallView: UIView {
         featureDelegate = delegate
         self.vertical = vertical
         self.horizontal = horizontal
-        addSubview(imageView)
         scrollview.bounces = false
         scrollview.isPagingEnabled = true
+        scrollview.delegate = self
+        scrollview.showsHorizontalScrollIndicator = false
         addSubview(scrollview)
-    }
-    
-    func setBackgroundImage(_ image: UIImage?) {
-        imageView.image = image
+        pageCon.numberOfPages = list.count%(vertical*horizontal) == 0 ? list.count/(vertical*horizontal) : list.count/(vertical*horizontal)+1
+        pageCon.frame.size = pageCon.size(forNumberOfPages: pageCon.numberOfPages)
+        pageCon.currentPageIndicatorTintColor = FeatureWall_PageControl_currentPageColor
+        pageCon.pageIndicatorTintColor = FeatureWall_PageControl_PageColor
+        pageCon.hidesForSinglePage = true
+        addSubview(pageCon)
     }
     
     func setContentList(_ list: [PlatformFeatureID] ) {
         featureIDList = list
         setFeatureWall(vertical, horizontal)
+        pageCon.numberOfPages = list.count%(vertical*horizontal) == 0 ? list.count/(vertical*horizontal) : list.count/(vertical*horizontal)+1
     }
 
     // MARK: - private
     private func setFeatureWall(_ vertical: Int, _ horizontal: Int)  {
         scrollview.subviews.forEach{ view in if view is FeatureWallCellView {view.removeFromSuperview()} }
         let page = CGFloat( featureIDList.count%(vertical*horizontal) == 0 ? featureIDList.count/(vertical*horizontal) : featureIDList.count/(vertical*horizontal)+1 )
-        scrollview.contentSize = CGSize(width: frame.width*page, height: frame.height)
+        scrollview.contentSize = CGSize(width: scrollview.frame.size.width*page, height: scrollview.frame.size.height)
         if vertical != 0 && horizontal != 0 {
-            let width = frame.width / CGFloat(vertical)
-            let height = frame.height / CGFloat(horizontal)
+            let width = scrollview.frame.size.width / CGFloat(vertical)
+            let height = scrollview.frame.size.height / CGFloat(horizontal)
             for current in 0...featureIDList.count-1 {
                 let position = getXYPositionByIndex(current, vertical, horizontal)
                 let wallCell = Platform.plat.getUIByID(.UIID_FeatureWallCell, self) as! FeatureWallCellView
-                wallCell.frame = CGRect(x: CGFloat(position.x)*width + CGFloat(position.page)*frame.width, y: CGFloat(position.y)*height, width: width, height: height)
+                wallCell.frame = CGRect(x: CGFloat(position.x)*width + CGFloat(position.page)*scrollview.frame.width, y: CGFloat(position.y)*height, width: width, height: height)
                 wallCell.imageView.image = UIImage(named: String(featureIDList[current].rawValue))
                 wallCell.titleLabel.text = Platform.plat.getFeatureNameByID(featureIDList[current])
                 wallCell.button.addTarget(self, action: #selector(clickFeatureBtn(_:)), for: .touchUpInside)
@@ -81,6 +90,11 @@ class FeatureWallView: UIView {
     // MARK: - selector
     func clickFeatureBtn(_ sender:UIButton)  {
         featureDelegate?.clickFeatureBtn(PlatformFeatureID(rawValue: sender.tag)!)
+    }
+    
+    // MARK: - UIScrollViewDelegate
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        pageCon.currentPage = Int(scrollview.contentOffset.x / UIScreen.main.bounds.size.width)
     }
 }
 
